f43e630ab0a87a1bf65ee8be501a6c90feff30c7
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-06-09 14:00:51 -0500

    Added sorting functionality to tables/dropdowns. Fixed visual bug with tables

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,10 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsChevronDown } from 'react-icons/bs';
 
 const Dropdown = ({ label, textData, onItemClicked }) => {
     const [isOpen, setIsOpen] = useState(false);
-    const [isAlphabetic, setIsAlphabetic] = useState(true);
+    const [sortState, setSortState] = useState(0);  // 0 for original, 1 for ascending, -1 for descending
+    const [sortedData, setSortedData] = useState([...textData]);
     const toggleOpen = () => setIsOpen(!isOpen);
+
+    useEffect(() => {
+        let newSortedData = [...textData];
+        if (sortState !== 0) {
+            newSortedData.sort((a, b) => {
+                if (!a.label || !b.label) {
+                    return;
+                }
+                const labelA = a.label.toUpperCase();
+                const labelB = b.label.toUpperCase();
+                if (labelA > labelB) {
+                    return sortState;
+                }
+                if (labelA < labelB) {
+                    return -sortState;
+                }
+                return 0;
+            });
+        }
+        setSortedData(newSortedData);
+    }, [textData, sortState]);
+
+    const toggleSort = () => {
+        setSortState((prevSortState) => (prevSortState === 0 ? 1 : prevSortState === 1 ? -1 : 0));
+    };
 
     return (
         <div className="">
@@ -13,15 +39,17 @@ const Dropdown = ({ label, textData, onItemClicked }) => {
                 <BsChevronDown size={30} className={`-translate-x-2 ml-auto transition ${isOpen ? 'rotate-180' : 'rotate-0'}`}/>
             </div>
             {isOpen && (
-                <div className="cursor-pointer bg-accent/50 text-white">
+                <div className="bg-gold/80 text-red-900 cursor-pointer">
                     <div>
-                        <div onClick={() => setIsAlphabetic(!isAlphabetic)} className="flex bg-accent/60s border-black border-2 py-2 pl-16 hover:bg-grey transition">
-                            <p className="font-semibold"> Named Glosses</p>
-                            <div className="flex pr-2 ml-auto">
+                        <div onClick={toggleSort} className="flex border-black border-2 py-2 pl-16 hover:bg-grey transition">
+                            <p className="font-semibold text-xl"> Named Glosses</p>
+                            <div className="flex pr-8 ml-auto">
                                 <p>
-                                    {isAlphabetic
+                                {sortState === 0
                                         ? "Sort from A to Z "
-                                        : "Sort from Z to A "    
+                                        : sortState === 1
+                                        ? "Sort from Z to A "
+                                        : "Original Order"    
                                     }
                                 </p>
                                 <p className="px-2">
@@ -29,7 +57,7 @@ const Dropdown = ({ label, textData, onItemClicked }) => {
                                 </p>
                             </div>
                         </div>
-                        {textData.map((item, index) => (
+                        {sortedData.map((item, index) => (
                             <div onClick={item['@id'] ? () => onItemClicked(item['@id'].split('/').pop(), item.label) : () => {}} key={index} className="border-black border-2 py-2 pl-16 hover:bg-grey transition">
                                 <div className="flex">
                                     <p>{item.label}</p>
--- components/GlossSheet.js
+++ components/GlossSheet.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 
-function TableComponent({ headers, data }) {
+function GlossSheet({ headers, data }) {
+    const initialSortingState = headers.map(header => false);
+    const [isAlphabetic, setIsAlphabetic] = useState(initialSortingState);
+    const [lastSortedIndex, setLastSortedIndex] = useState(null);
+    const [sortedData, setSortedData] = useState([...data]);
+
+    const toggleSort = (index) => {
+        const newSortingState = headers.map((header, i) => i === index ? !isAlphabetic[i] : false);
+        setIsAlphabetic(newSortingState);
+        setLastSortedIndex(index);
+        sortData(headers[index], isAlphabetic[index]);
+    }
+
+    const sortData = (header, isAlphabetic) => {
+        const newData = [...sortedData].sort((a, b) => {
+            if (isAlphabetic) {
+                return a[header] > b[header] ? 1 : -1;
+            } else {
+                return a[header] < b[header] ? 1 : -1;
+            }
+        });
+        setSortedData(newData);
+    }
+
     return (
         <div className="rounded-lg">
-            <div className={`text-2xl bg-accent text-yellow-200 cursor-pointer grid grid-cols-${headers.length} border-black font-bold`}>
+            <div className={`text-2xl bg-accent text-yellow-200 cursor-pointer grid grid-cols-2 border-black font-bold`}>
                 {headers.map((value, index) => (
-                    <div key={index} className="hover:bg-darkGrey transition flex border border-black p-2">
+                    <div onClick={() => toggleSort(index)} key={index} className={`hover:bg-darkGrey transition flex border border-black p-2 ${index === lastSortedIndex ? 'opacity-70' : ''}`}>
                         {value} 
-                        <div className="pl-2 ml-auto">↑↓</div>
+                        <div className="flex text-xl translate-y-1 ml-auto">
+                            <p>
+                                {isAlphabetic[index]
+                                    ? "Sort from Z to A "
+                                    : "Sort from A to Z "    
+                                }
+                            </p>
+                            <p className="px-2">
+                                ↑↓
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
-            {data.map((row, index) => (
-                <div className={`grid grid-cols-${headers.length} border-black`}  key={index}>
+            {sortedData.map((row, index) => (
+                <div className={`grid grid-cols-2 border-black`}  key={index}>
                     {headers.map((header, i) => (
-                        <div className="border border-black p-2" key={i}>{row[header]}</div>
+                        <div className={`bg-gold/80 text-red-900 border border-black p-2 ${i === lastSortedIndex ? 'opacity-70' : ''}`} key={i}>{row[header]}</div>
                     ))}
                 </div>
             ))}
@@ -22,4 +55,4 @@ function TableComponent({ headers, data }) {
     );
 }
 
-export default TableComponent;
+export default GlossSheet;
