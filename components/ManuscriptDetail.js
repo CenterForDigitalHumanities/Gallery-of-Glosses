9ac48f3537a9c5d2618687b0af13b09e6f4bb69f
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-06-20 10:13:53 -0500

    Map coordinates fixed. Markers appearance get darker as more manuscripts appear from marker location. When clicked, now correctly displays the information of manuscripts in modal to the side.

---
+++ components/ManuscriptDetail.js
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+const ManuscriptDetail = ({ item, onBack }) => {
+    const placeholderTranscription = "This is where the transcription of the Gloss text will be.";
+    const placeholderImage = "https://via.placeholder.com/200";  
+    const placeholderRelatedGlosses = [
+        { title: "Gloss 1", link: "#" },
+        { title: "Gloss 2", link: "#" },
+    ];
+    const placeholderExternalLinks = [
+        { title: "External Text 1", link: "#" },
+        { title: "External Text 2", link: "#" },
+    ];
+    const placeholderCanonicalText = "This is where the canonical text related to the gloss will be.";
+    
+    const { label, pageData } = item;
+
+    return (
+        <div className="py-4 bg-gray-50">
+            <h1 className="text-4xl mb-4">{label}</h1>
+            <div className="border-[1.5px] border-black bg-grey/10 mb-4 rounded-md p-2">
+                <div className="px-2 flex gap-2">
+                    <Link href="/"><p className="text-blue-500 font-semibold hover:underline">Home</p></Link> 
+                    &gt; <p className="text-blue-500 font-semibold cursor-pointer hover:underline" onClick={onBack} >{pageData.name} </p>
+                    &gt; <p className="font-semibold">{label}</p>
+                </div>
+            </div>
+
+            <div className="flex justify-between">
+                <div className="w-1/2 pr-4">
+                    <h2 className="text-2xl mb-2 text-blue-700">Transcription:</h2>
+                    <p className="bg-white border rounded-md p-3 shadow-md">{placeholderTranscription}</p>
+                </div>
+
+                <div className="w-1/2 pl-4">
+                    <h2 className="text-2xl mb-2 text-blue-700">Gloss Image:</h2>
+                    <img className="shadow-lg rounded-md" src={placeholderImage} alt="Gloss image"/>
+                </div>           
+            </div>
+            
+            <div className="mt-6">
+                <h2 className="text-2xl mb-2">Related Glosses:</h2>
+                <div className="flex flex-col">
+                    {placeholderRelatedGlosses.map((Gloss, index) => 
+                    <div key={index} className="mb-1"><Link href={Gloss.link}><p className="text-blue-600 hover:text-blue-800">{Gloss.title}</p></Link></div>
+                    )}
+                </div>
+            </div>
+
+            <div className="mt-6">
+                <h2 className="text-2xl mb-2">External Texts and Entities:</h2>
+                <div className="flex flex-col">
+                    {placeholderExternalLinks.map((link, index) => 
+                    <div key={index} className="mb-1"><Link href={link.link}><p className="text-blue-600 hover:text-blue-800">{link.title}</p></Link></div>
+                    )}
+                </div>
+            </div>
+
+            <div className="mt-6">
+            <h2 className="text-2xl mb-2">Description:</h2>
+            <p className="bg-white border rounded-md p-3 shadow-md">{placeholderCanonicalText}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ManuscriptDetail;
--- components/MapMarkerModal.js
+++ components/MapMarkerModal.js
@@ -1,31 +1,40 @@
 import { useEffect, useState } from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const MapMarkerModal = ({ marker, visible, onClose }) => {
-	const [isVisible, setIsVisible] = useState(!!visible);
+    const [isVisible, setIsVisible] = useState(!!visible);
+    console.log
+    useEffect(() => {
+        setIsVisible(!!visible)
+    }, [visible]);
 
-	useEffect(() => {
-		setIsVisible(!!visible)
-	}, [visible]);
-
-	if (!isVisible) {
-		return null;
-	}
-
-    if (!marker) {
+    if (!isVisible) {
         return null;
     }
 
-    const { 'body.alternative.value': siglum, 'body.city.value': city, 'body.date.value': date } = marker;
-
-    console.log("marker", marker)
-
-	return (
+    if (!marker || marker.length === 0) {
+        return null;
+    }
+    
+    return (
         <div className="w-full border-2 p-1 mt-2 border-black rounded shadow-lg flex flex-col">
-            <p>Siglum: {siglum}</p>
-            <p>City: {city}</p>
-            <p>Date: {date}</p>
+            {marker.length > 0 && marker[0]['body.city.value'] && <p className="mb-2 text-2xl font-bold underline">{marker[0]['body.city.value']}:</p>}
+
+            {marker.map(({ 'body.alternative.value': siglum, 'body.city.value': city, 'body.date.value': date }, index) => (
+                <div key={index}>
+                    <p>Siglum: {siglum}</p>
+                    <p>City: {city}</p>
+                    <p>Date: {date? date : "No Date Recorded"}</p>
+                    <button onClick={()=>{}} className="bg-black/30 text-white rounded-md mb-2 py-1 px-2 w-auto text-xs lg:text-lg font-semibold flex flex-rows items-center hover:bg-black/20 transition">
+                        <AiOutlineInfoCircle className="mr-1" />
+                        More Info
+                    </button>
+                    <hr className="border-black"/> {/* Optional separator */}
+                </div>
+            ))}
+            <button onClick={onClose} className="mt-4 px-4 py-2 bg-grey text-white rounded hover:bg-black/20 transition">Close</button>
         </div>
-	)
+    )
 }
 
 export default MapMarkerModal;
