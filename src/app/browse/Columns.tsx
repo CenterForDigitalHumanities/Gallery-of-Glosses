0ff3a7ea4f54a294afa9285d9982fa5f2693940f
Tu Tran <ttran46@slu.edu>
2023-10-16 15:51:28 -0500

    fixed build errors by creating new file for cell content

--- src/app/browse/Columns.tsx
+++ src/app/browse/Columns.tsx
@@ -3,6 +3,7 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "./DataTableColumnHeader";
 import { useState } from "react";
+import DataTableCell from "./DataTableCell";
 
 export const columns: ColumnDef<ProcessedGloss>[] = [
   {
@@ -34,35 +35,8 @@ export const columns: ColumnDef<ProcessedGloss>[] = [
       <DataTableColumnHeader column={column} title="Target Text" />
     ),
     cell: ({ row }) => {
-      const [showMoreRows, setShowMoreRows] = useState(new Set<string>());
-      const textValue: string = row.getValue("textValue");
-      const isShowMore = showMoreRows.has(row.id);
-
-      const limit = 20;
-      const shortText =
-        textValue.substring(0, 100) + (textValue.length > limit ? "..." : "");
-      const displayText = isShowMore ? textValue : shortText;
-
       return (
-        <div className="font-medium">
-          {displayText}
-          {textValue.length > limit && (
-            <button
-              className="text-xs hover:underline text-muted-foreground "
-              onClick={() => {
-                const newShowMoreRows = new Set(showMoreRows);
-                if (isShowMore) {
-                  newShowMoreRows.delete(row.id);
-                } else {
-                  newShowMoreRows.add(row.id);
-                }
-                setShowMoreRows(newShowMoreRows);
-              }}
-            >
-              {isShowMore ? " Show Less" : " Show More"}
-            </button>
-          )}
-        </div>
+        <DataTableCell textValue={row.getValue("textValue")} rowId={row.id} />
       );
     },
   },
---
+++ src/app/browse/DataTableCell.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+
+interface DataTableCellProps {
+  textValue: string;
+  rowId: string;
+}
+
+const DataTableCell: React.FC<DataTableCellProps> = ({ textValue, rowId }) => {
+  const [showMoreRows, setShowMoreRows] = useState(new Set<string>());
+  const isShowMore = showMoreRows.has(rowId);
+
+  const limit = 20;
+  const shortText =
+    textValue.substring(0, 100) + (textValue.length > limit ? "..." : "");
+  const displayText = isShowMore ? textValue : shortText;
+
+  return (
+    <div className="font-medium">
+      {displayText}
+      {textValue.length > limit && (
+        <button
+          className="text-xs hover:underline text-muted-foreground"
+          onClick={() => {
+            const newShowMoreRows = new Set(showMoreRows);
+            if (isShowMore) {
+              newShowMoreRows.delete(rowId);
+            } else {
+              newShowMoreRows.add(rowId);
+            }
+            setShowMoreRows(newShowMoreRows);
+          }}
+        >
+          {isShowMore ? " Show Less" : " Show More"}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default DataTableCell;
