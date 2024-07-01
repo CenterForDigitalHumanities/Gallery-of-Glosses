776aec579688fd7f4d1a939657dcd2b7f9e1fce4
Leandru Martin <leandru.martin@slu.edu>
2024-07-01 15:03:50 -0500

    Basic implementation of new more flexible Columns.tsx

--- src/app/browse/(site)/page.tsx
+++ src/app/browse/(site)/page.tsx
@@ -5,13 +5,16 @@ import { columns } from "../Columns";
 import { DataTable } from "../DataTable";
 import { useGlossList } from "@/hooks/useGlossList";
 
+import { make_columns } from "../Columns copy";
+let columnsNew = make_columns([{ header: "Incipit", accessorKey: "title" }, { header: "Target Chapter", accessorKey: "targetChapter" }, { header: "Target Verse", accessorKey: "targetVerse" }, { header: "Target Text", accessorKey: "textValue" }])
+
 interface BrowseAllGlossesProps {}
 
 const BrowseAllGlosses: FC<BrowseAllGlossesProps> = ({}) => {
   const { glosses, loading } = useGlossList();
   return (
     <div>
-      {<DataTable columns={columns} data={glosses} loading={loading} />}
+      {<DataTable columns={columnsNew} data={glosses} loading={loading} />}
     </div>
   );
 };
