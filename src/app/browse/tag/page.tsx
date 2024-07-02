6aa719c24f8ee65bb0ee30578e203ee0a5977bb3
Leandru Martin <leandru.martin@slu.edu>
2024-07-01 20:59:57 -0500

    Get list of tags

--- src/app/browse/tag/page.tsx
+++ src/app/browse/tag/page.tsx
@@ -13,6 +13,18 @@ let columns = make_columns([
 
 const Tag: FC<TagProps> = ({}) => {
   const { glosses, loading } = useGlossList();
+
+  let tags: string[] = []
+  tags = glosses.map((gloss) => {
+    if (gloss.tags)
+      return gloss.tags.split(' ').filter(tag =>
+        !tags.includes(tag)
+      )
+    else
+      return [];
+  })
+    .flat();
+
   return (
     <div>
       {<DataTable columns={columns} data={glosses} loading={loading} />}
