18f91ad98e5599d21c2f168a1041beae1ee1a49e
Leandru Martin <leandru.martin@slu.edu>
2024-07-06 15:00:14 -0500

    Edited gloss display page: removed unnecessary fields; added in description, canonical reference, and themes; and added more helpful placeholder text.

--- src/app/gloss/[slug]/page.tsx
+++ src/app/gloss/[slug]/page.tsx
@@ -16,51 +16,43 @@ const GlossInstance = () => {
     <div>
       <div className="text-foreground p-4 md:p-8">
         <h1 className={`text-2xl font-bold mb-4 ${!gloss && blurredStyles}`}>
-          {gloss ? gloss.title : "4913490110934"}
+          {gloss && gloss.title ? gloss.title : "Not found"}
         </h1>
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
           <p>
-            <span className="font-semibold">Collection:</span>{" "}
+            <span className="font-semibold">Canonical Reference Locator:</span>{" "}
             <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.targetCollection : "4913490110934"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Chapter:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.targetChapter : "5"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Verse:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.targetVerse : "2"}
+              {gloss && gloss.canonicalReference ? gloss.canonicalReference : "Not found"}
             </span>
           </p>
           <p>
             <span className="font-semibold">Language:</span>{" "}
             <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.textLanguage : "73"}
+              {gloss && gloss.textLanguage ? gloss.textLanguage : "Not found"}
             </span>
           </p>
           <p>
-            <span className="font-semibold">Format:</span>{" "}
+            <span className="font-semibold">Description:</span>{" "}
             <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.textFormat : "491349/110934"}
+              {gloss && gloss.description ? gloss.description : "Not found"}
             </span>
           </p>
           <p>
             <span className="font-semibold">Tags:</span>{" "}
             <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.tags : "4913, 4901, 10934"}
+              {gloss && gloss.tags ? gloss.tags : "Not found"}
+            </span>
+          </p>
+          <p>
+            <span className="font-semibold">Themes:</span>{" "}
+            <span className={`${!gloss && blurredStyles}`}>
+              {gloss && gloss.themes ? gloss.themes : "Not found"}
             </span>
           </p>
         </div>
         <div className="rounded-xl shadow-inner">
           <p className={`text-justify ${!gloss && blurredStyles}`}>
-            {gloss
-              ? gloss.textValue
-              : "Ex dolore ipsum quis pariatur nulla proident exercitation exercitation aliqua dolor dolor est cillum. Fugiat enim non reprehenderit in laborum voluptate ut exercitation et esse. Qui cupidatat irure in officia eu consectetur amet mollit exercitation excepteur fugiat occaecat in aute. Mollit nisi aliqua exercitation labore minim sunt ut id voluptate aliqua magna deserunt. Lorem magna ipsum culpa dolor ea veniam fugiat id officia. Irure qui culpa eiusmod laborum velit sunt velit ut anim consectetur anim mollit Lorem."}
+            {gloss && gloss.textValue ? gloss.textValue : "Not found"}
           </p>
         </div>
       </div>
