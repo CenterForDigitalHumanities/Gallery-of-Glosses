79877bc0bbba148a66e6391bfe2c5b9755cb466a
Leandru Martin <leandru.martin@slu.edu>
2024-08-03 15:09:24 -0500

    Display attached Glosses

--- src/app/transcription/[slug]/page.tsx
+++ src/app/transcription/[slug]/page.tsx
@@ -3,69 +3,81 @@
 import { RERUM } from "@/configs/rerum-links";
 import { useGlossInstance } from "@/hooks/useGlossInstance";
 import { usePathname } from "next/navigation";
+import { useTranscriptionInstance } from "@/hooks/useTranscriptionInstance";
+
+function ReferencesListItems(props: {
+  reference: string;
+  referenceIndex: number;
+  maxReferences: number;
+}) {
+  const gloss = useGlossInstance(props.reference);
+  const glossId = props.reference.split("/").pop();
+  return (
+    <a href={`/gloss/${glossId}`}>
+      {gloss
+        ? gloss.title +
+          (props.referenceIndex < props.maxReferences - 1 ? ", " : "")
+        : "Loading..."}
+    </a> /* TODO: style this as a link */
+  );
+}
 
 const TranscriptionInstance = () => {
   const pathname = usePathname();
 
   const targetId = RERUM + pathname.split("/transcription/")[1];
-  // const transcription = useGlossInstance(targetId);
+  const transcription = useTranscriptionInstance(targetId);
 
   const blurredStyles = "filter blur-md opacity-50";
 
   return (
     <div>
       <div className="text-foreground p-4 md:p-8">
-        <h1 className={`text-2xl font-bold mb-4 ${blurredStyles}`}>
+        <h1
+          className={`text-2xl font-bold mb-4 ${!transcription && blurredStyles}`}
+        >
           {transcription ? transcription.title : "Not found"}
         </h1>
-      </div>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
-        <p>
-          <span className="font-semibold">Shelfmark:</span>{" "}
-          <span className={`${!transcription && blurredStyles}`}>
-            {transcription ? transcription.identifier : "Not found"}
-          </span>
-        </p>
-        <p>
-          <span className="font-semibold">Text:</span>{" "}
-          <span className={`${!transcription && blurredStyles}`}>
-            {transcription ? transcription.textValue : "Not found"}
-          </span>
-        </p>
-        <p>
-          <span className="font-semibold">Language:</span>{" "}
-          <span className={`${!transcription && blurredStyles}`}>
-            {transcription ? transcription.textLanguage : "Not found"}
-          </span>
-        </p>
-        <p>
-          <span className="font-semibold">Language:</span>{" "}
-          <span className={`${!transcription && blurredStyles}`}>
-            {transcription ? transcription.textLanguage : "Not found"}
-          </span>
-        </p>
-        <p>
-          <span className="font-semibold">Attached Glosses:</span>{" "}
-          <span className={`${!transcription && blurredStyles}`}>
-            {transcription ? (
-              <ul>
-                {transcription.references.map((reference) => {
-                  let gloss = useGlossInstance(reference);
-                  let glossId = reference.split("/").pop();
-                  return (
-                    <li>
-                      <a href={`/gloss/${glossId}`}>
-                        gloss ? gloss.title : "Loading..."
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-            ) : (
-              "Not found"
-            )}
-          </span>
-        </p>
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
+          <p>
+            <span className="font-semibold">Shelfmark:</span>{" "}
+            <span className={`${!transcription && blurredStyles}`}>
+              {transcription ? transcription.identifier : "Not found"}
+            </span>
+          </p>
+          <p>
+            <span className="font-semibold">Language:</span>{" "}
+            <span className={`${!transcription && blurredStyles}`}>
+              {transcription ? transcription.textLanguage : "Not found"}
+            </span>
+          </p>
+          <p>
+            <span className="font-semibold">Attached Glosses:</span>{" "}
+            <span className={`${!transcription && blurredStyles}`}>
+              {transcription
+                ? transcription.references &&
+                  transcription.references.map((reference, referenceIndex) => {
+                    return (
+                      <ReferencesListItems
+                        key={reference}
+                        reference={reference}
+                        referenceIndex={referenceIndex}
+                        maxReferences={transcription?.references?.length ?? 0}
+                      />
+                    );
+                  })
+                : "Not found"}
+            </span>
+          </p>
+        </div>
+        <div className="rounded-xl shadow-inner">
+          <p>
+            <span className="font-semibold">Text:</span>{" "}
+            <span className={`${!transcription && blurredStyles}`}>
+              {transcription ? transcription.textValue : "Not found"}
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
