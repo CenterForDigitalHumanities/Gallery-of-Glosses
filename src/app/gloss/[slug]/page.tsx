98a11bebf5b7cf0e37e57a0d5994a9e12b95aa01
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-10-25 17:17:24 -0500

    pop the ids

--- src/app/gloss/[slug]/page.tsx
+++ src/app/gloss/[slug]/page.tsx
@@ -1,13 +1,10 @@
 
 import Gloss from "./Gloss.tsx"
 
-// FIXME we really need to do the 'type' annotations here so these aren't seen as 'any'
-// FIXME this isn't working.  We get ' Page "/witness/[slug]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.'
-// We get that error because this script couldn't actually generate the dynamic pages.  gloss is undefined, bad map()?
 export async function generateStaticParams() {
   const glosses = await fetch('https://store.rerum.io/v1/id/610c54deffce846a83e70625').then((res) => res.json()).then((j) => j.itemListElement)
   let ids = glosses.map((gloss) => { 
-    return {slug:gloss["@id"]}
+    return {slug:gloss["@id"].split("/").pop()}
   })
   return ids
 }
--- src/app/witness/[slug]/page.tsx
+++ src/app/witness/[slug]/page.tsx
@@ -1,13 +1,10 @@
 
 import Witness from "./Witness.tsx"
 
-// FIXME we really need to do the 'type' annotations here so these aren't seen as 'any'
-// FIXME this isn't working.  We get ' Page "/witness/[slug]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.'
-// We get that error because this script couldn't actually generate the dynamic pages.  man is undefined, bad map()?
 export async function generateStaticParams() {
   const manuscripts = await fetch('https://store.rerum.io/v1/id/610ad6f1ffce846a83e70613').then((res) => res.json()).then((j) => j.itemListElement)
   let ids = manuscripts.map((man) => { 
-    return {slug:man["@id"]}
+    return {slug:man["@id"].split.pop()}
   })
   return ids
 }
