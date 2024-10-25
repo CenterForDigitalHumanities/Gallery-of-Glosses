28ed99b6f29fa127ce5f9064b56d80ef7c05bea7
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-10-25 16:05:33 -0500

    I do not like this at all.  I wish we weren't doing typescript and nextJS.  oh well.

--- src/app/gloss/[slug]/page.tsx
+++ src/app/gloss/[slug]/page.tsx
@@ -1,156 +1,19 @@
-"use client";
 
-import { RERUM } from "@/configs/rerum-links";
-import { useGlossInstance } from "@/hooks/useGlossInstance";
-import { usePathname } from "next/navigation";
-import { make_columns } from "@/app/browse/Columns";
-import { DataTable } from "@/app/browse/DataTable";
-import { useGlossWitnesses } from "@/hooks/useGlossWitnesses";
+import Gloss from "./Gloss.tsx"
 
-const filterColumn = {
-  header: "Witness",
-  accessorKey: "identifier",
-  expandable: false,
-};
-const columns = make_columns([
-  filterColumn,
-  {
-    header: "Witness Details (under construction)",
-    accessorKey: "city",
-    expandable: false,
-  },
-]);
+// FIXME we really need to do the 'type' annotations here so these aren't seen as 'any'
+// FIXME this isn't working.  We get ' Page "/witness/[slug]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.'
+// We get that error because this script couldn't actually generate the dynamic pages.  gloss is undefined, bad map()?
+export async function generateStaticParams() {
+  const glosses = await fetch('https://store.rerum.io/v1/id/610c54deffce846a83e70625').then((res) => res.json()).then((j) => j.itemListElement)
+  return glosses.map((gloss) => {
+    id:gloss["@id"]
+  })
+}
 
-const parseGlossText = (text: string) => {
-  const parser = new DOMParser();
-  let doc = parser.parseFromString(text, "text/html");
-
-  // Replace each <SEG ref="..."> with italicized text
-  const segElements = doc.querySelectorAll("SEG");
-  segElements.forEach((segElement) => {
-    // Add reference after text
-    let refSpan = document.createElement("span");
-    refSpan.textContent = ` (${segElement.getAttribute("ref") ?? "No reference found"})`;
-    segElement.insertAdjacentElement("afterend", refSpan);
-
-    // Change text to italic
-    let span = document.createElement("span");
-    span.style.fontStyle = "italic";
-    span.textContent = segElement.textContent;
-    segElement.replaceWith(span);
-  });
-
-  // Replace each <Target text="..."> with <span style="font-weight: bold" title="...">
-  const targetElements = doc.querySelectorAll("Target");
-  targetElements.forEach((targetElement) => {
-    // Add reference after text
-    let refSpan = document.createElement("span");
-    refSpan.textContent = ` (${targetElement.getAttribute("text") ?? "No reference found"})`;
-    targetElement.insertAdjacentElement("afterend", refSpan);
-
-    // Change text to bold
-    let span = document.createElement("span");
-    span.style.fontWeight = "bold";
-    span.textContent = targetElement.textContent;
-    targetElement.replaceWith(span);
-  });
-
-  return doc.body.innerHTML;
-};
-
-const GlossInstance = () => {
-  const pathname = usePathname();
-
-  const targetId = RERUM + pathname.split("/gloss/")[1];
-  const gloss = useGlossInstance(targetId);
-  const witnessesResult = useGlossWitnesses(targetId);
-  let witnesses = witnessesResult.witnesses;
-
-  const blurredStyles = "filter blur-md opacity-50";
-
-  return (
-    <div>
-      <div className="text-foreground p-4 md:p-8">
-        <h1 className={`text-2xl font-bold mb-4 ${!gloss && blurredStyles}`}>
-          {gloss && gloss.title ? gloss.title : "Not found"}
-        </h1>
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
-          <p>
-            <span className="font-semibold">Canonical Reference Locator:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss && gloss.canonicalReference
-                ? gloss.canonicalReference
-                : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Language:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss && gloss.textLanguage ? gloss.textLanguage : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Description:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss && gloss.description ? gloss.description : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Tags:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss && gloss.tags
-                ? gloss.tags.map((tag, tagIndex, tagArray) => (
-                    <a
-                      key={tagIndex}
-                      href={"/browse/tag?q=" + tag}
-                      className="text-blue-500 hover:underline"
-                    >
-                      {tag}
-                      {tagIndex < tagArray.length - 1 ? ", " : ""}
-                    </a>
-                  ))
-                : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Themes:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss && gloss.themes
-                ? gloss.themes
-                    .join("")
-                    .split(", ")
-                    .map(
-                      (theme, themeIndex, themeArray) =>
-                        theme +
-                        (themeIndex < themeArray.length - 1 ? ", " : ""),
-                    )
-                : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Target Text:</span>{" "}
-            <span className={`${!gloss && blurredStyles}`}>
-              {gloss ? gloss.targetedText : "Not found"}
-            </span>
-          </p>
-        </div>
-        <div className="rounded-xl shadow-inner">
-          <p className={`text-justify ${!gloss && blurredStyles}`}>
-            {gloss && gloss.textValue ? gloss.textValue : "Not found"}
-          </p>
-        </div>
-        <h2 className={`text-xl font-bold mb-4 ${!witnesses && blurredStyles}`}>
-          Witness References
-        </h2>
-        <DataTable
-          columns={columns}
-          data={witnesses}
-          loading={witnessesResult.loading}
-          filterColumn={filterColumn}
-        />
-      </div>
-    </div>
-  );
-};
+const GlossInstance = async ({ params }: { params: { id: string }}) => {
+  const id = await params
+  return <Gloss slug={params.id}/>
+}
 
 export default GlossInstance;
--- src/app/witness/[slug]/page.tsx
+++ src/app/witness/[slug]/page.tsx
@@ -1,99 +1,19 @@
-"use client";
 
-import { RERUM } from "@/configs/rerum-links";
-import { useWitnessInstance } from "@/hooks/useWitnessInstance";
-import { usePathname } from "next/navigation";
-import { make_columns } from "../../browse/Columns";
-import { DataTable } from "../../browse/DataTable";
-import { useWitnessGlossesList } from "@/hooks/useWitnessGlossesList";
+import Witness from "./Witness.tsx"
 
-let filterColumn = {
-  header: "Incipit",
-  accessorKey: "title",
-  expandable: false,
-};
-let columns = make_columns([
-  filterColumn,
-  {
-    header: "Canonical Reference Locator",
-    accessorKey: "canonicalReference",
-    expandable: false,
-  },
-  { header: "Gloss Text", accessorKey: "textValue", expandable: true },
-]);
+// FIXME we really need to do the 'type' annotations here so these aren't seen as 'any'
+// FIXME this isn't working.  We get ' Page "/witness/[slug]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.'
+// We get that error because this script couldn't actually generate the dynamic pages.  man is undefined, bad map()?
+export async function generateStaticParams() {
+  const manuscripts = await fetch('https://store.rerum.io/v1/id/610ad6f1ffce846a83e70613').then((res) => res.json()).then((j) => j.itemListElement)
+  return manuscripts.map((man) => {
+    id:man["@id"]
+  })
+}
 
-const WitnessInstance = () => {
-  const pathname = usePathname();
-
-  const targetId = RERUM + pathname.split("/witness/")[1];
-  const witness = useWitnessInstance(targetId);
-
-  const blurredStyles = "filter blur-md opacity-50";
-
-  let { glosses, loading } = useWitnessGlossesList(witness?.identifier);
-
-  return (
-    <div>
-      <div className="text-foreground p-4 md:p-8">
-        <h1 className={`text-2xl font-bold mb-4 ${!witness && blurredStyles}`}>
-          {witness ? witness.identifier : "Not found"}
-        </h1>
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
-          <p>
-            <span className="font-semibold">Repository:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.repository : "Not found"}
-            </span>
-          </p>{" "}
-          <p>
-            <span className="font-semibold">City:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.city : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Institution:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.institution : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Provenance:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.provenance : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Region:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.region : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Data URL:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.url : "Not found"}
-            </span>
-          </p>
-          <p>
-            <span className="font-semibold">Base Project:</span>{" "}
-            <span className={`${!witness && blurredStyles}`}>
-              {witness ? witness.baseProject : "Not found"}
-            </span>
-          </p>
-        </div>
-        <h2 className={`text-xl font-bold mb-4 ${!witness && blurredStyles}`}>
-          Attached Glosses
-        </h2>
-        <DataTable
-          columns={columns}
-          data={glosses}
-          loading={loading}
-          filterColumn={filterColumn}
-        />
-      </div>
-    </div>
-  );
+const WitnessInstance = async ({ params }: { params: { id: string }}) => {
+  const id = await params
+  return <Witness slug={params.id}/>
 };
 
 export default WitnessInstance;
