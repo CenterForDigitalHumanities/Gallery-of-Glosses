92b890d655e3bcfe089a3885382811bc0e486e13
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-10-25 11:22:42 -0500

    advanced tactics

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //output: 'export',
+  //output: 'standalone'
+  reactStrictMode: true,
+  basePath: '/Gallery-of-Glosses',
+  distDir: 'site',
+  crossOrigin: 'anonymous',
+}
+
+export default nextConfig;
